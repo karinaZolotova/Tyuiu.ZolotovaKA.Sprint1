6ee72d6ec9dcba37102c52c2b114dee88fc1aea5
Карина Алексеевна Золотова <karinovirys19@gmail.com>
2024-10-02 21:33:54 +0500

    Task0

--- Tyuiu.ZolotovaKA.Sprint1.Task0.V0/Program.cs
+++ Tyuiu.ZolotovaKA.Sprint1.Task0.V0/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tyuiu.ZolotovaKA.Sprint1.Task0.V20.Lib;
-
+// 
 namespace Tyuiu.ZolotovaKA.Sprint1.Task0.V20
 {
     internal class Program
